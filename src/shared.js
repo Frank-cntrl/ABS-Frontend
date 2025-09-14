49581e21e85d173608aae9fed68c1217b5427eb2
Franccesco Petta <franccescopettamuller@gmail.com>
2025-09-14 15:46:36 -0400

    env update

--- src/shared.js
+++ src/shared.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.API_URL || "http://localhost:8080";
-export const SOCKETS_URL = process.env.SOCKETS_URL || "ws://localhost:8080";
+export const API_URL = process.env.REACT_APP_API_URL || "https://abs-backend-nine.vercel.app";
+export const SOCKETS_URL = process.env.REACT_APP_SOCKETS_URL || "wss://abs-backend-nine.vercel.app";
 export const NODE_ENV = process.env.NODE_ENV || "development";
